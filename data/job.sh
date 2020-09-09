92ffb0f43713134291e48a18459b1b7e120d55ff
HY <yan@dyn3166-190.wlan.ic.ac.uk>
2020-09-09 19:32:43 +0100

    Timing

--- data/job.sh
+++ data/job.sh
@@ -2,11 +2,17 @@
 
 # shellcheck disable=SC2039
 # shellcheck disable=SC2034
-for ((n=0;n<1;n++))
-do
- mkdir outputs
- ./run_sample.py Estonia --threads 8 --outputdir outputs
- rm -rf outputs
+
+for i in {1..10}; do
+  mkdir t"$i"
+  for ((n=0;n<9;n++))
+  do
+    mkdir outputs
+    ./run_sample.py Estonia --threads $i --outputdir outputs
+    rm -rf outputs
+  done
+  mkdir outputs
+  ./run_sample.py Estonia --threads $i --outputdir outputs
+  mv outputs t"$i"
+  mv Timing.txt t"$i"
 done
- mkdir outputs
-./run_sample.py Estonia --threads 8 --outputdir outputs
