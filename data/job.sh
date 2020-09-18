130a84a7dbe3f9efc3340c5689a60ec9fac58b4f
HYCJX <2768272676@qq.com>
2020-09-18 01:46:02 +0100

    Timing Script

---
+++ data/job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# shellcheck disable=SC2039
+# shellcheck disable=SC2034
+
+for i in {1..10}; do
+  mkdir t"$i"
+  for ((n==0;n<9;n++))
+  do
+    mkdir outputs
+    ./run_sample.py Estonia --threads $i --outputdir outputs
+    rm -rf outputs
+  done
+  mkdir outputs
+  ./run_sample.py Estonia --threads $i --outputdir outputs
+  mv outputs t"$i"
+  mv Timing.txt t"$i"
+done
